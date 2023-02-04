5c8a248d617a82ae8486b8ba0a24ee64bfb0789a
Shade2012 <ubasanino@gmail.com>
2023-02-04 12:34:56 +0700

    style:[S2] add relative_layout1

--- app/src/main/java/com/example/contraintandrelativelayout/MainActivity2.java
+++ app/src/main/java/com/example/contraintandrelativelayout/MainActivity2.java
@@ -1,13 +1,25 @@
 package com.example.contraintandrelativelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        btnLogin = (Button) findViewById(R.id.ganti);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity2.this,MainActivity.class ));
+            }
+        });
     }
 }
