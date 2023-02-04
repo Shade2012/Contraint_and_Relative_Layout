3f1fa3612a9c9aec6ed856769247a7e29fd535a7
Shade2012 <ubasanino@gmail.com>
2023-02-04 09:20:11 +0700

    initial commit

--- app/src/main/java/com/example/contraintandrelativelayout/MainActivity2.java
+++ app/src/main/java/com/example/contraintandrelativelayout/MainActivity2.java
@@ -1,7 +1,6 @@
 package com.example.contraintandrelativelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity2 extends AppCompatActivity {
