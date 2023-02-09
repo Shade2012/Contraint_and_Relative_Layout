db5e2e3717946832958333c5a3d4da3dec47577f
izal24 <izalizzudin18@gmail.com>
2023-02-09 18:57:41 +0700

    feat:[I4] add constrainlayout

--- app/src/main/java/com/example/contraintandrelativelayout/MainActivity.java
+++ app/src/main/java/com/example/contraintandrelativelayout/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.contraintandrelativelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button pesan;
+    Button kembali2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pesan = findViewById(R.id.pesan);
+        kembali2 = findViewById(R.id.kembali);
+        pesan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Pesanan sedang di proses " , Toast.LENGTH_SHORT).show();
+            }
+        });
+        kembali2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,MainActivity2.class ));
+            }
+        });
     }
 }
